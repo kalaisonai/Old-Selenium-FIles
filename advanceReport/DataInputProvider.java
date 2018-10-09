2507d77c5deb194c08a9363b829f12129aea8c7d
TestLeafPages <koushik.chatterjee@testleaf.com>
2018-10-09 13:17:38 +0530

    Add files via upload

---
+++ advanceReport/DataInputProvider.java
@@ -0,0 +1,58 @@
+package utils;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataInputProvider{
+
+	public static Object[][] getSheet(String dataSheetName) {		
+
+		Object[][] data = null ;
+
+		try {			
+			XSSFWorkbook workbook = new XSSFWorkbook("./data/"+dataSheetName+".xlsx");
+			XSSFSheet sheet = workbook.getSheetAt(0);	
+
+			// get the number of rows
+			int rowCount = sheet.getLastRowNum();
+			// get the number of columns
+			int columnCount = sheet.getRow(0).getLastCellNum();
+
+			data = new String[rowCount][columnCount];
+
+			// loop through the rows
+			for(int i=1; i <rowCount+1; i++){
+				try {
+					XSSFRow row = sheet.getRow(i);
+					for(int j=0; j <columnCount; j++){ // loop through the columns
+						try {
+							String cellValue = "";
+							try{
+								if (row.getCell(j).getCellTypeEnum()==CellType.STRING) {
+									cellValue = row.getCell(j).getStringCellValue();
+								} else if (row.getCell(j).getCellTypeEnum()==CellType.NUMERIC) {
+									cellValue = ""+row.getCell(j).getNumericCellValue();
+								}
+							}catch(NullPointerException e){
+
+							}
+							data[i-1][j]  = cellValue; // add to the data array
+						} catch (Exception e) {
+							e.printStackTrace();
+						}				
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}	
+			workbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return data;
+	}
+}
